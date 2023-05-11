741f3143cc4ebfb339b8ebb0a2a1a09a775a74d3
Mason Blanford <blanform@flip3.engr.oregonstate.edu>
2023-05-10 18:40:28 -0700

    added fields, manipulation queries, research_papers_has_authors

---
+++ README.md
@@ -0,0 +1,7 @@
+# Changes
+- From Research_Papers:
+--- Removed citation_id, since Citations entity captures the 1:M
+--- Removed author_id, since Authors entity captures the M:N
+
+- Institutions:
+--- Added city, phone columns
---
+++ database/dml.sql
@@ -0,0 +1,114 @@
+--------------------------------------
+----- Research_Papers Operations -----
+--------------------------------------
+
+----- Create research paper -----
+-- Create title
+[SQL QUERY]
+-- Read (Select), add date
+[SQL QUERY]
+-- Read (Select) institution from dropdown
+[SQL QUERY]
+-- Read (Select) discipline from dropdown
+[SQL QUERY]
+-- Create DOI
+[SQL QUERY]
+-- Submit on click
+[SQL QUERY]
+
+----- Update research paper -----
+[SQL QUERY]
+
+----- Delete research paper -----
+[SQL QUERY]
+
+--------------------------------
+----- Citations Operations -----
+--------------------------------
+
+----- Create citation -----
+-- Read (Select) citing paper from dropdown
+[SQL QUERY]
+-- Read (Select) referenced paper from dropdown
+[SQL QUERY]
+-- Submit on click
+[SQL QUERY]
+
+----- Update citation -----
+[SQL QUERY]
+
+----- Delete citation -----
+[SQL QUERY]
+
+------------------------------
+----- Authors Operations -----
+------------------------------
+
+----- Create author -----
+-- Create first name
+[SQL QUERY]
+-- Create surname
+[SQL QUERY]
+-- Submit on click
+[SQL QUERY]
+
+----- Update author -----
+[SQL QUERY]
+
+----- Delete author -----
+[SQL QUERY]
+
+--------------------------------------------------
+----- Research_Papers_has_Authors Operations -----
+--------------------------------------------------
+
+----- Create multiple authors to research paper -----
+-- Read (Select) author from dropdown
+[SQL QUERY]
+-- Read (Select) research paper from dropdown
+[SQL QUERY]
+-- Submit on click
+[SQL QUERY]
+
+----- Update association -----
+[SQL QUERY]
+
+----- Delete association -----
+[SQL QUERY]
+
+----------------------------------
+----- Institutions Operations -----
+----------------------------------
+
+----- Create institution -----
+-- Create name
+[SQL QUERY]
+-- Create address
+[SQL QUERY]
+-- Create city
+[SQL QUERY]
+-- Create country
+[SQL QUERY]
+-- Create phone
+[SQL QUERY]
+-- Create website
+
+----- Update institution -----
+[SQL QUERY]
+
+----- Delete institution -----
+[SQL QUERY]
+
+----------------------------------
+----- Disciplines Operations -----
+----------------------------------
+
+----- Create discipline -----
+-- Create field
+[SQL QUERY]
+
+----- Update discipline -----
+[SQL QUERY]
+
+----- Delete discipline -----
+[SQL QUERY]

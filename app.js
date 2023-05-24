fd1727a4898d8f5232a31a5f168166c91a8b570f
Whitney Zilton <ziltonw@flip2.engr.oregonstate.edu>
2023-05-23 20:12:00 -0700

    added ajax for adding an author

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-PORT = 1991;
+PORT = 1993;
 
 // database
 var db = require("./database/db-connector");
@@ -76,85 +76,73 @@ app.get("/authors", function (req, res) {
 //////////////////////////////////////////////////////////////////////////////////////
 
 // POST ROUTES
-app.post("/add-author-form", function (req, res) {
-  // parse incoming data back to JavaScript object
-  let data = req.body;
-
-  // // catch NULL values
-  // let homeworld = parseInt(data['input-homeworld']);
-  // if (isNaN(homeworld))
-  // {
-  //     homeworld = 'NULL'
-  // }
-
-  // let age = parseInt(data['input-age']);
-  // if (isNaN(age))
-  // {
-  //     age = 'NULL'
-  // }
-
-  // run query on database
-  query1 = `INSERT INTO Authors (first_name, last_name) VALUES ("${data["input-first_name"]}", "${data["input-last_name"]}")`;
-  db.pool.query(query1, function (error, rows, fields) {
-    // check if error
-    if (error) {
-      // log error, send 400
-      console.log(error);
-      res.sendStnatus(400);
-    }
-
-    // if no error: redirect to root, run and show SELECT * FROM Authors
-    else {
-      res.redirect("/authors");
-    }
-  });
-});
-
-// app.post("/add-author-ajax", function (req, res) {
+// //*****NOTE**** Route below works****
+// app.post("/add-author-form", function (req, res) {
 //   // parse incoming data back to JavaScript object
 //   let data = req.body;
 
 //   // // catch NULL values
-//   // let homeworld = parseInt(data.homeworld);
+//   // let homeworld = parseInt(data['input-homeworld']);
 //   // if (isNaN(homeworld))
 //   // {
 //   //     homeworld = 'NULL'
 //   // }
 
-//   // let age = parseInt(data.age);
+//   // let age = parseInt(data['input-age']);
 //   // if (isNaN(age))
 //   // {
 //   //     age = 'NULL'
 //   // }
 
 //   // run query on database
-//   query1 = `INSERT INTO Authors (first_name, last_name) VALUES ("${data.first_name}", "${data.last_name}")`;
+//   query1 = `INSERT INTO Authors (first_name, last_name) VALUES ("${data["input-first_name"]}", "${data["input-last_name"]}")`;
 //   db.pool.query(query1, function (error, rows, fields) {
 //     // check if error
 //     if (error) {
-//       // log error to terminal, send HTTP response 400 showing bad request
+//       // log error, send 400
 //       console.log(error);
-//       res.sendStatus(400);
+//       res.sendStnatus(400);
 //     }
 
-//     // if no error, run SELECT * on Authors
+//     // if no error: redirect to root, run and show SELECT * FROM Authors
 //     else {
-//       query2 = `SELECT * FROM Authors;`;
-//       db.pool.query(query2, function (error, rows, fields) {
-//         // if error on second query, send 400
-//         if (error) {
-//           console.log(error);
-//           res.sendStatus(400);
-//         }
-
-//         // else good, send query
-//         else {
-//           res.send(rows);
-//         }
-//       });
+//       res.redirect("/authors");
 //     }
 //   });
 // });
+
+app.post("/add-author-ajax", function (req, res) {
+  // parse incoming data back to JavaScript object
+  let data = req.body;
+
+  // run query on database
+  query1 = `INSERT INTO Authors (first_name, last_name) VALUES ('${data.first_name}', '${data.last_name}')`;
+  db.pool.query(query1, function (error, rows, fields) {
+    // check if error
+    if (error) {
+      // log error to terminal, send HTTP response 400 showing bad request
+      console.log(error);
+      res.sendStatus(400);
+    }
+
+    // if no error, run SELECT * on Authors
+    else {
+      query2 = `SELECT * FROM Authors;`;
+      db.pool.query(query2, function (error, rows, fields) {
+        // if error on second query, send 400
+        if (error) {
+          console.log(error);
+          res.sendStatus(400);
+        }
+
+        // else good, send query
+        else {
+          res.send(rows);
+        }
+      });
+    }
+  });
+});
 
 /* LISTENER */
 app.listen(PORT, function () {
--- public/js/add_author.js
+++ public/js/add_author.js
@@ -1,76 +1,80 @@
-// // get objects to alter
-// let addAuthorForm = document.getElementById("add-author-form-ajax");
+// get objects to alter
+let addAuthorForm = document.getElementById("add-author-form-ajax");
 
-// // alter needed objects
-// addPersonForm.addEventListener("submit", function (e) {
-//   // don't submit form
-//   e.preventDefault();
+// alter needed objects
+addAuthorForm.addEventListener("submit", function (e) {
+  // don't submit form
+  e.preventDefault();
 
-//   // get forms for data retrieval
-//   let inputFirstName = document.getElementById("input-first_name");
-//   let inputLastName = document.getElementById("input-last_name");
+  // get forms for data retrieval
+  let inputFirstName = document.getElementById("input-first_name");
+  let inputLastName = document.getElementById("input-last_name");
 
-//   // get form values
-//   let firstNameValue = inputFirstName.value;
-//   let lastNameValue = inputLastName.value;
+  // get form values
+  let firstNameValue = inputFirstName.value;
+  let lastNameValue = inputLastName.value;
+  console.log(firstNameValue, lastNameValue);
 
-//   // convert data to JavaScript object
-//   let data = {
-//     first_name: firstNameValue,
-//     last_name: lastNameValue,
-//   };
+  // convert data to JavaScript object
+  let data = {
+    first_name: firstNameValue,
+    last_name: lastNameValue,
+  };
 
-//   // prep AJAX request
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.open("POST", "/add-author-ajax", true);
-//   xhttp.setRequestHeader("Content-type", "application/json");
+  // prep AJAX request
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/add-author-ajax", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
 
-//   // tell AJAX request to resolve
-//   xhttp.onreadystatechange = () => {
-//     if (xhttp.readyState == 4 && xhttp.status == 200) {
-//       // add new data to table
-//       addRowToTable(xhttp.response);
+  // tell AJAX request to resolve
+  xhttp.onreadystatechange = () => {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      // add new data to table
+      addRowToTable(xhttp.response);
 
-//       // clear inputs for new transaction
-//       inputFirstName.value = "";
-//       inputLastName.value = "";
-//     } else if (xhttp.readyState == 4 && xhttp.status != 200) {
-//       console.log("There was an error with the input.");
-//     }
-//   };
+      // clear inputs for new transaction
+      inputFirstName.value = '';
+      inputLastName.value = '';
+    } 
+    else if (xhttp.readyState == 4 && xhttp.status != 200) {
+      console.log("There was an error with the input.");
+    }
+  };
 
-//   // send request, wait on response
-//   xhttp.send(JSON.stringify(data));
-// });
+  // send request, wait on response
+  xhttp.send(JSON.stringify(data));
+});
 
-// // write one row to Object — i.e., a single Authors record
-// addRowToTable = (data) => {
-//   // Get reference to current table on page, clear it
-//   let currentTable = document.getElementById("authors-table");
+// write one row to Object — i.e., a single Authors record
+addRowToTable = (data) => {
+  // Get reference to current table on page, clear it
+  let currentTable = document.getElementById("authors-table");
 
-//   // find where to insert new row — i.e., the last table row
-//   let newRowIndex = currentTable.rows.length;
+  // find where to insert new row — i.e., the last table row
+  let newRowIndex = currentTable.rows.length;
 
-//   // get reference to new row from database query — i.e., the last object
-//   let parsedData = JSON.parse(data);
-//   let newRow = parsedData[parsedData.length - 1];
+  // get reference to new row from database query — i.e., the last object
+  let parsedData = JSON.parse(data);
+  let newRow = parsedData[parsedData.length - 1];
 
-//   // create row, three cells
-//   let row = document.createElement("TR");
-//   let idCell = document.createElement("TD");
-//   let firstNameCell = document.createElement("TD");
-//   let lastNameCell = document.createElement("TD");
+  // create row, three cells
+  let row = document.createElement("TR");
+  let idCell = document.createElement("TD");
+  let firstNameCell = document.createElement("TD");
+  let lastNameCell = document.createElement("TD");
 
-//   // write correct data to cells
-//   idCell.innerText = newRow.id;
-//   firstNameCell.innerText = newRow.first_name;
-//   lastNameCell.innerText = newRow.last_name;
+  // write correct data to cells
+  // change below to 
+  idCell.innerText = newRow.author_id;
+  //idCell.innerText = newRow.id;
+  firstNameCell.innerText = newRow.first_name;
+  lastNameCell.innerText = newRow.last_name;
 
-//   // add cells to row
-//   row.appendChild(idCell);
-//   row.appendChild(firstNameCell);
-//   row.appendChild(lastNameCell);
+  // add cells to row
+  row.appendChild(idCell);
+  row.appendChild(firstNameCell);
+  row.appendChild(lastNameCell);
 
-//   // add row to table
-//   currentTable.appendChild(row);
-// };
+  // add row to table
+  currentTable.appendChild(row);
+};

4b48267aec901b85f832a8e419180cecce4266d3
Whitney Zilton <ziltonw@flip1.engr.oregonstate.edu>
2023-05-24 20:46:18 -0700

    made some changes; update still isnt working

--- public/js/add_author.js
+++ public/js/add_author.js
@@ -89,7 +89,7 @@ addRowToTable = (data) => {
   // add row to table
   currentTable.appendChild(row);
 
-  // Start of new Step 8 code for adding new data to the dropdown menu for updating people
+  // Start of new Step 8 code for adding new data to the dropdown menu for updating authors
   
   // Find drop down menu, create a new option, fill data in the option (full name, id),
   // then append option to drop down menu so newly created rows via ajax will be found in it without needing a refresh

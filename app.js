1fc0f0f1331f4b76efdfbb30c02cd714f650beca
Whitney Zilton <ziltonw@flip2.engr.oregonstate.edu>
2023-05-30 22:36:36 -0700

    add Institution works; updated website type in html form to be text, not url

--- app.js
+++ app.js
@@ -174,6 +174,47 @@ app.post('/add-discipline-ajax', function (req, res) {
   })
 });
 
+// app.js - ROUTES section
+
+app.post('/add-institution-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+    console.log("this is data", data);
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Institutions (name, address, city, country, website) VALUES ('${data.name}', '${data.address}', '${data.city}', '${data.country}', '${data.website}')`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on Institutions
+            query2 = `SELECT * FROM Institutions;`;
+            db.pool.query(query2, function(error, rows, fields){
+
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
 app.put("/put-author-ajax", function (req, res, next) {
   let data = req.body;
   let author = parseInt(data.author_id);
